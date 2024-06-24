09be8bd4c96420e464052bef5add9ec71ef93ed2
NAmirov <NAmirov@elordaeco.kz>
2024-06-24 16:22:17 +0500

    limit for upload up to 100 mb

--- strapi-app/config/api.js
+++ strapi-app/config/api.js
@@ -1,13 +1,20 @@
 module.exports = {
-  settings: {
-    parser: {
-      enabled: true,
-      multipart: true,
-      formidable: {
-        maxFileSize: 104857600, // Sets the file size limit to 10MB
+  upload: {
+    config: {
+      providerOptions: {
+        sizeLimit: 104857600, // 20MB in bytes
       },
     },
   },
+  // settings: {
+  //   parser: {
+  //     enabled: true,
+  //     multipart: true,
+  //     formidable: {
+  //       maxFileSize: 104857600,
+  //     },
+  //   },
+  // },
   rest: {
     defaultLimit: 100,
     maxLimit: 150,
