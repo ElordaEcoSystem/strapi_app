e59d118ba6557b5e50a699e09c53fce3dfbdfd8f
NAmirov <NAmirov@elordaeco.kz>
2024-06-25 08:11:00 +0500

    little fix

--- strapi-app/config/plugins.js
+++ strapi-app/config/plugins.js
@@ -14,7 +14,7 @@ module.exports = ({ env }) => ({
       sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
       providerOptions: {
         localServer: {
-          maxage: 300000,
+          maxage: 104857600, //300000
         },
       },
     },
