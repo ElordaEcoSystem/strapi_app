76424ad22f5fa52f75ba8e943e1c876a289268d8
NAmirov <NAmirov@elordaeco.kz>
2024-06-25 07:55:41 +0500

    up upload limit

--- strapi-app/config/plugins.js
+++ strapi-app/config/plugins.js
@@ -1,8 +1,21 @@
-module.exports = () => ({
+// module.exports = () => ({
+//   upload: {
+//     config: {
+//       providerOptions: {
+//         sizeLimit: 104857600, // 20MB in bytes
+//       },
+//     },
+//   },
+// });
+
+module.exports = ({ env }) => ({
   upload: {
     config: {
+      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
       providerOptions: {
-        sizeLimit: 104857600, // 20MB in bytes
+        localServer: {
+          maxage: 300000,
+        },
       },
     },
   },
